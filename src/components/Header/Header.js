72b46d1a09deb3b51146d9aeb8af101507d3a1c1
alamin500 <nalamin500@gmail.com>
2021-10-24 11:18:09 +0600

    h

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Header.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Header.css";
 
 const Header = () => {
-    return (
-        <div className="header">
-            <nav>
-                <Link to="/">Home</Link>
-                <Link to="/users">Users</Link>
-                <Link to="/users/add">Add User</Link>
-            </nav>
-        </div>
-    );
+  return (
+    <div className="header">
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/users">Users</Link>
+        <Link to="/users/add">Add User</Link>
+        <Link to="/users/update/:id">Update Users</Link>
+      </nav>
+    </div>
+  );
 };
 
 export default Header;
--- src/components/UpdateUser/UpdateUser.js
+++ src/components/UpdateUser/UpdateUser.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import userEvent from "@testing-library/user-event";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router";
 
 const UpdateUser = () => {
-    return (
-        <div>
-            <h2>This is Update User</h2>
-        </div>
-    );
+  const [user, setUser] = useState({});
+  const { id } = useParams;
+  useEffect(() => {
+    const url = `http://localhost:5000/users/${id}`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setUser(data));
+  }, []);
+
+  const handleUpdateUser = (e) => {};
+  return (
+    <div>
+      <h2>Update User{user.name}</h2>
+      <p>
+        <small>{id}</small>
+      </p>
+      <form onSubmit={handleUpdateUser}>
+        <input type="text" />
+        <input type="email" name="" id="" />
+        <input type="submit" value="Update" />
+      </form>
+    </div>
+  );
 };
 
 export default UpdateUser;
